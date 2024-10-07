86150a39ce7be45fab2f7124aa77718f46308cab
Abhijith BS <vivekabi1717@gmail.com>
2024-10-07 21:30:49 +0530

    modified:   src/services/userApi.js

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,7 +1,7 @@
  import axios from 'axios';
 
 // const BASE_URL = "http://localhost:4000/api/v1/user";
-const apiUrl = import.meta.env.VITE_API_URL || 'cineticketsbook-server.vercel.app/api/v1';
+const apiUrl = import.meta.env.VITE_API_URL || 'cineticketsbook-server.vercel.app/';
 
 
 
