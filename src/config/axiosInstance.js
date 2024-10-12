112fbb42532e371e5c4ff1ceb70861653dbc0d02
Abhijith BS <vivekabi1717@gmail.com>
2024-10-12 12:09:00 +0530

    #	modified:   src/config/axiosInstance.js

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);  
 
-const baseURL = import.meta.env.VITE_API_URL.replace(/\/$/, '');  
+const baseURL = import.meta.env.VITE_API_URL ;  
 export const axiosInstance = axios.create({
   baseURL: `${baseURL}/api/v1`,
   withCredentials: true,
